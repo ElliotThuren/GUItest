1d2b3439463789c9dcf6982c8dd9c9850de57c26
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-02-09 11:19:21 +0100

    9/2-2021

---
+++ src/guii.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class guii {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+}
