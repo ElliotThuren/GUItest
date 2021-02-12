7b60f9c4dd0d500452085bf0d1a12b2993b7d809
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-02-12 11:21:03 +0100

    12/9-2021

--- src/guii.java
+++ src/guii.java
@@ -1,11 +1,43 @@
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class guii {
     private JTextArea textArea;
     private JPanel panel1;
     private JButton Nytt;
-    private JButton Öpnna;
+    private JButton Oppna;
     private JButton Spara;
+
+    public guii() {
+        Nytt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textArea.setText("");
+            }
+        });
+        Oppna.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        Spara.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("guii");
+        frame.setTitle("TextField");
+        frame.setContentPane(new guii().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(1900,1000);
+    }
 }
