a062c21e436829f8d85f353d2444c18bb46e5376
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-02-19 11:23:54 +0100

    19/9-2021

--- src/guii.java
+++ src/guii.java
@@ -1,7 +1,9 @@
+import jdk.nashorn.internal.ir.IfNode;
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class guii {
     private JTextArea textArea;
@@ -14,6 +16,9 @@ public class guii {
         Nytt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                //JOptionPane.showInputDialog("Är du säger på att du vill rensa detta dokument? I så fall skriv: rensa");
+
+                //JOptionPane.showMessageDialog("Nu är det rensat.");
                 textArea.setText("");
             }
         });
@@ -21,11 +26,45 @@ public class guii {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
-            }
+                        String filename = JOptionPane.showInputDialog("")+(".txt");
+                        FileReader fr = null;
+                        try {
+                            fr = new FileReader(filename);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        BufferedReader inFile = new BufferedReader(fr);
+
+                        String line;
+                        try {
+                            while ((line = inFile.readLine()) != null) {
+                                textArea.append(line);
+                            }
+                            inFile.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
         });
         Spara.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                String text = textArea.getText();
+                System.out.println(text);
+                String filename2 = JOptionPane.showInputDialog("")+(".txt");
+                FileWriter fw = null;
+                try {
+                    fw = new FileWriter(filename2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter outFile = new PrintWriter(bw);
+                        outFile.println(text);
+                    outFile.flush();
+                    outFile.close();
+
 
             }
         });
